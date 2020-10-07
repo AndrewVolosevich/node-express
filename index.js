27cd9c97fcef5d18dd299badee2ec900f60d4e7c
AndrewVolosevich <volosevich1989@gmail.com>
2020-10-07 12:08:30 +0300

    add form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
 const PORT = process.env.PORT || 5555
 
 const app = express()
@@ -14,25 +17,10 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home page',
-    isHome: true
-  })
-})
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Courses',
-    isCourses: true
-  })
-})
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Add course',
-    isAdd: true
-  })
-})
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port = ${PORT}`);
